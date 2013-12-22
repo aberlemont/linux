638efac4f17a46b296c72ee684671acf0ad8f7a3
Alexis Berlemont <alexis.berlemont@gmail.com>
2013-12-23 00:33:27 +0100

    perf config: replace NO_PERF_REGS with CONFIG_PERF_REGS

--- tools/perf/util/perf_regs.h
+++ tools/perf/util/perf_regs.h
@@ -1,7 +1,9 @@
 #ifndef __PERF_REGS_H
 #define __PERF_REGS_H
 
-#ifdef HAVE_PERF_REGS_SUPPORT
+#include <linux/kconfig.h>
+
+#ifdef CONFIG_PERF_REGS
 #include <perf_regs.h>
 #else
 #define PERF_REGS_MASK	0
